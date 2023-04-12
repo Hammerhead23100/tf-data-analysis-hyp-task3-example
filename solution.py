1b094d74431c03773eacab6c9da51958f3b7b66d
Hammerhead23100 <88268741+Hammerhead23100@users.noreply.github.com>
2023-04-13 05:39:02 +0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,10 +6,7 @@ import scipy.stats as sps
 chat_id = 254567352 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    p_value = sps.permutation_test((x, y), lambda x, y, axis: np.mean(x, axis=axis) - np.mean(y, axis=axis),
-                                   vectorized=True,
-                                   n_resamples=5000,
-                                   alternative='less').pvalue
+    p_value = sps.permutation_test((x, y), lambda x, y, axis: np.mean(x, axis=axis) - np.mean(y, axis=axis), vectorized=True, n_resamples=5000, alternative='greater').pvalue
     alpha = 0.05
     # Измените код этой функции
     # Это будет вашим решением
